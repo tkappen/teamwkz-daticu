34729fd2a85478a45f04a3bb22534a57c6736ba1
jnijman <joppenijman@gmail.com>
2018-01-20 19:38:30 +0100

    Update drug extraction.sql

--- drug extraction.sql
+++ drug extraction.sql
@@ -1,15 +1,50 @@
 /* identify drugs with quantities */
 
-SELECT
-  drug_source_concept_id
-, drug_source_value
-, "sum"(quantity) sum_quantity
-, dose_unit_source_value
+SELECT  concept_id
 , concept_name
+, "sum"(quantity) sum_quantity
+, LOWER(dose_unit_source_value) AS unit
 FROM
   (drug_exposure e
 INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
-WHERE (drug_source_value LIKE '%epinephrine%') /* insert drug name here */
-GROUP BY drug_source_concept_id, drug_source_value, dose_unit_source_value, concept_name
+WHERE   domain_id = 'Drug'
+GROUP BY concept_id, concept_name, LOWER(dose_unit_source_value)
 HAVING ("sum"(quantity) <> 0)
-ORDER BY drug_source_value DESC
+ORDER BY sum_quantity DESC
+
+/* quantitative fluids (ml or ml/hour) */
+SELECT  concept_id
+, concept_name
+, "sum"(quantity) sum_quantity
+, LOWER(dose_unit_source_value) AS unit
+FROM
+  (drug_exposure e
+INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
+WHERE   domain_id = 'Drug' AND
+        (LOWER(dose_unit_source_value) LIKE 'ml' OR LOWER(dose_unit_source_value) LIKE 'ml/hour')
+GROUP BY concept_id, concept_name, LOWER(dose_unit_source_value)
+HAVING ("sum"(quantity) <> 0)
+ORDER BY sum_quantity DESC
+
+/* fluids per patient with start and end datetime */
+/* !!! LIMIT in place */
+
+SELECT e.person_id
+, e.drug_exposure_start_datetime
+, e.drug_exposure_end_datetime
+, concept_id
+, concept_name
+, "sum"(quantity) sum_quantity
+, LOWER(dose_unit_source_value) AS unit
+, route_source_value
+FROM
+  (drug_exposure e
+INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
+WHERE   domain_id = 'Drug' AND
+        (LOWER(dose_unit_source_value) LIKE 'ml' OR LOWER(dose_unit_source_value) LIKE 'ml/hour') AND
+        e.person_id = 62074985 AND
+        LOWER(route_source_value) LIKE '%iv%'
+GROUP BY e.person_id, concept_id, concept_name, LOWER(dose_unit_source_value), e.drug_exposure_start_datetime, e.drug_exposure_end_datetime, route_source_value
+HAVING ("sum"(quantity) <> 0)
+ORDER BY sum_quantity DESC
+LIMIT 100
