0c8c61107805a7296d2f3f24f5e747b1bd681196
jnijman <joppenijman@gmail.com>
2018-01-21 08:18:13 +0100

    Update drug extraction.sql

--- drug extraction.sql
+++ drug extraction.sql
@@ -27,24 +27,20 @@ HAVING ("sum"(quantity) <> 0)
 ORDER BY sum_quantity DESC
 
 /* fluids per patient with start and end datetime */
-/* !!! LIMIT in place */
-
 SELECT e.person_id
 , e.drug_exposure_start_datetime
 , e.drug_exposure_end_datetime
 , concept_id
 , concept_name
-, "sum"(quantity) sum_quantity
+, quantity
 , LOWER(dose_unit_source_value) AS unit
 , route_source_value
 FROM
   (drug_exposure e
 INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
-WHERE   domain_id = 'Drug' AND
+WHERE   quantity <> 0 AND
+        domain_id = 'Drug' AND
         (LOWER(dose_unit_source_value) LIKE 'ml' OR LOWER(dose_unit_source_value) LIKE 'ml/hour') AND
-        e.person_id = 62074985 AND
         LOWER(route_source_value) LIKE '%iv%'
-GROUP BY e.person_id, concept_id, concept_name, LOWER(dose_unit_source_value), e.drug_exposure_start_datetime, e.drug_exposure_end_datetime, route_source_value
-HAVING ("sum"(quantity) <> 0)
-ORDER BY sum_quantity DESC
-LIMIT 100
+GROUP BY e.person_id, concept_id, concept_name, LOWER(dose_unit_source_value), e.drug_exposure_start_datetime, e.drug_exposure_end_datetime, route_source_value, quantity
+ORDER BY quantity DESC
