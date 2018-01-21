9b0448d3ffe98eb9a0bc6690cd1ab002bee76e64
jnijman <joppenijman@gmail.com>
2018-01-21 09:32:31 +0100

    Update drug extraction.sql

--- drug extraction.sql
+++ drug extraction.sql
@@ -1,5 +1,4 @@
 /* identify drugs with quantities */
-
 SELECT  concept_id
 , concept_name
 , "sum"(quantity) sum_quantity
@@ -7,7 +6,8 @@ SELECT  concept_id
 FROM
   (drug_exposure e
 INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
-WHERE   domain_id = 'Drug'
+WHERE   domain_id = 'Drug' AND
+        concept_name LIKE '%epinephrine%'
 GROUP BY concept_id, concept_name, LOWER(dose_unit_source_value)
 HAVING ("sum"(quantity) <> 0)
 ORDER BY sum_quantity DESC
@@ -26,7 +26,7 @@ GROUP BY concept_id, concept_name, LOWER(dose_unit_source_value)
 HAVING ("sum"(quantity) <> 0)
 ORDER BY sum_quantity DESC
 
-/* fluids per patient with start and end datetime */
+/* fluids in ml per patient with start and end datetime */
 SELECT e.person_id
 , e.drug_exposure_start_datetime
 , e.drug_exposure_end_datetime
@@ -40,7 +40,10 @@ FROM
 INNER JOIN concept c ON (e.drug_concept_id = c.concept_id))
 WHERE   quantity <> 0 AND
         domain_id = 'Drug' AND
-        (LOWER(dose_unit_source_value) LIKE 'ml' OR LOWER(dose_unit_source_value) LIKE 'ml/hour') AND
+        (LOWER(dose_unit_source_value) LIKE 'ml') AND
         LOWER(route_source_value) LIKE '%iv%'
 GROUP BY e.person_id, concept_id, concept_name, LOWER(dose_unit_source_value), e.drug_exposure_start_datetime, e.drug_exposure_end_datetime, route_source_value, quantity
 ORDER BY quantity DESC
+
+/* fluids from ml/hour calculated as ml per patient with start and end datetime */
+
