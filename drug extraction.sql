d51e77552ee44c8ec7b5db258729c92377a8f660
WoBaDijk <wouter@van-dijk.nu>
2018-01-21 10:34:13 +0100

    Added drugs pumps in mg's administered

--- drug extraction.sql
+++ drug extraction.sql
@@ -45,5 +45,38 @@ WHERE   quantity <> 0 AND
 GROUP BY e.person_id, concept_id, concept_name, LOWER(dose_unit_source_value), e.drug_exposure_start_datetime, e.drug_exposure_end_datetime, route_source_value, quantity
 ORDER BY quantity DESC
 
-/* fluids from ml/hour calculated as ml per patient with start and end datetime */
-
+/* pumps from mcg/kg/min calculated as mg per patient with start and end datetime */
+SELECT person_id
+, drug
+, quantity
+, start_datetime
+, end_datetime
+, date_diff
+, unit
+, (quantity * ((seconds + (minutes * 60) + (hours * 3600) + (days * 86400)) / 60) / 1000) as mg_administered
+, administration_route
+FROM (
+    SELECT de.person_id
+    , de.drug_exposure_start_datetime as start_datetime
+    , de.drug_exposure_end_datetime as end_datetime
+    , de.drug_exposure_end_datetime - de.drug_exposure_start_datetime as date_diff
+    , EXTRACT(SECOND from (de.drug_exposure_end_datetime - de.drug_exposure_start_datetime)) as seconds
+    , EXTRACT(MINUTE from (de.drug_exposure_end_datetime - de.drug_exposure_start_datetime)) as minutes
+    , EXTRACT(HOUR from (de.drug_exposure_end_datetime - de.drug_exposure_start_datetime)) as hours
+    , EXTRACT(DAY from (de.drug_exposure_end_datetime - de.drug_exposure_start_datetime)) as days
+    , co.concept_id
+    , co.concept_name as drug
+    , de.quantity
+    , LOWER(de.dose_unit_source_value) AS unit
+    , de.route_source_value as administration_route
+    FROM
+      (drug_exposure de
+        INNER JOIN concept co ON (de.drug_concept_id = co.concept_id))
+    WHERE   de.quantity <> 0 AND
+            co.domain_id = 'Drug' AND
+            (LOWER(de.dose_unit_source_value) = 'mcg/kg/min') AND
+            LOWER(de.route_source_value) LIKE '%iv%'
+    GROUP BY de.person_id, co.concept_id, co.concept_name, LOWER(de.dose_unit_source_value), de.drug_exposure_start_datetime, de.drug_exposure_end_datetime, de.route_source_value, de.quantity
+    ORDER BY de.quantity DESC
+)
+WHERE seconds + minutes + hours + days > 0
